28b2a5c9d80630927129d1fdc8418480df6db81e
Stefan Müller zzeroo <co@zzeroo.com>
2019-05-06 10:39:10 +0200

    First, basic, weston configuration with RA-GAS wallpaper
    This commit adds a very basic weston.ini with a wallpaper configured.
    The wallpaper is the one from RA-GAS GmbH.

---
+++ custom/package/xmz_weston_config/xmz_weston_config.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# xmz_weston_config
+#
+################################################################################
+
+XMZ_WESTON_CONFIG_VERSION = 0.1.0
+XMZ_WESTON_CONFIG_SITE = ../custom/package/xmz_weston_config
+XMZ_WESTON_CONFIG_SITE_METHOD = local
+XMZ_WESTON_CONFIG_LICENSE = GPL-2.0+
+XMZ_WESTON_CONFIG_LICENSE_FILES = COPYING
+
+XMZ_WESTON_CONFIG_DEPENDENCIES = weston systemd
+
+
+define XMZ_WESTON_CONFIG_INSTALL_TARGET_CMDS
+		$(INSTALL) -D -m 0644 $(@D)/weston.ini \
+				$(TARGET_DIR)/etc/xdg/weston/weston.ini
+		$(INSTALL) -D -m 0644 $(@D)/wallpaper-desktop-ra-gas.png \
+				$(TARGET_DIR)/usr/share/backgrounds/ra-gas/wallpaper-desktop-ra-gas.png
+endef
+
+$(eval $(generic-package))

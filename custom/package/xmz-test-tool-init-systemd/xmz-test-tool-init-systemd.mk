eb799b28036e5db6f2e75d206580f588ccaafe36
Stefan Müller zzeroo <co@zzeroo.com>
2019-05-07 23:02:36 +0200

    Rename all custom packages, used dashes instead of underscores

--- custom/package/xmz-test-tool-init-systemd/xmz-test-tool-init-systemd.mk
+++ custom/package/xmz-test-tool-init-systemd/xmz-test-tool-init-systemd.mk
@@ -5,12 +5,12 @@
 ################################################################################
 
 XMZ_TEST_TOOL_INIT_SYSTEMD_VERSION = 0.1.0
-XMZ_TEST_TOOL_INIT_SYSTEMD_SITE = ../custom/package/xmz_test_tool_init_systemd
+XMZ_TEST_TOOL_INIT_SYSTEMD_SITE = ../custom/package/xmz-test-tool-init-systemd
 XMZ_TEST_TOOL_INIT_SYSTEMD_SITE_METHOD = local
 XMZ_TEST_TOOL_INIT_SYSTEMD_LICENSE = GPL-2.0+
 XMZ_TEST_TOOL_INIT_SYSTEMD_LICENSE_FILES = COPYING
 
-XMZ_TEST_TOOL_INIT_SYSTEMD_DEPENDENCIES = weston systemd xmz_test_tool
+XMZ_TEST_TOOL_INIT_SYSTEMD_DEPENDENCIES = weston systemd xmz-test-tool
 
 
 define XMZ_TEST_TOOL_INIT_SYSTEMD_INSTALL_INIT_SYSTEMD
